2590e55ff3f6808f627e580cb751000ace32505f
alespo10 <alesposato15@gmail.com>
2024-11-06 12:21:11 +0100

    Aggiornamento Mer 6 novembre

--- Preprocessing/preprocessing_dataset.py
+++ Preprocessing/preprocessing_dataset.py
@@ -4,23 +4,9 @@ from Utility.log_help import log
 
 
 def load_and_convert_log_to_txt(input_csv_path, activities_to_remove=None):
-    """
-    Loads a CSV log file, converts each event into a descriptive sentence, and saves it to a text file.
-
-    Args:
-        input_csv_path (str): Full path of the CSV file to load.
-        dataset_name (str): Dataset name (e.g., "helpdesk", "sepsis") to select the right template.
-        activities_to_remove (list, optional): List of activities to exclude from the log.
-
-    Raises:
-        Exception: If the CSV file or specified dataset is not found.
-    """
-
     dataset_name = os.path.splitext(os.path.basename(input_csv_path))[0]
-
-    # Controllo per vedere se il dataset_name esiste nel log_help
     if dataset_name not in log:
-        raise ValueError(f"Dataset '{dataset_name}' not supported! Choose from {list(log.keys())}.")
+        raise ValueError(f"FileCSV '{dataset_name}' not supported! Choose from {list(log.keys())}.")
 
     # Extract template and configurations for the dataset
     template_info = log[dataset_name]
@@ -29,28 +15,23 @@ def load_and_convert_log_to_txt(input_csv_path, activities_to_remove=None):
     event_attributes = template_info['event_attribute']
     trace_attributes = template_info['trace_attribute']
 
-    # Load data from CSV file
+
     if not os.path.exists(input_csv_path):
         raise Exception(f"CSV file ({input_csv_path}) not found!")
     data_frame = pd.read_csv(input_csv_path, sep=",")
 
-    # Remove specified activities if provided
     if activities_to_remove is not None:
         data_frame = data_frame[~data_frame["activity"].isin(activities_to_remove)]
 
-    # Initialize list for sentences
     sentences = []
-
-    # Variable to keep track of the current case
     current_case = None
 
     # Loop through each event in the DataFrame
     for _, row in data_frame.iterrows():
         # Check if the case has changed
-        if row['case'] != current_case:         #Cambio per poter scrivere la traccia
-            current_case = row['case']  # Update current case
-
-            # Generate trace-level description using trace_template, if available
+        if row['case'] != current_case:
+            current_case = row['case']
+            # Generate trace-level description using trace_template
             if trace_template:
                 # Create a dictionary with only the trace attributes present in the DataFrame
                 trace_data = {attr: row[attr] for attr in trace_attributes if attr in row}
@@ -58,13 +39,10 @@ def load_and_convert_log_to_txt(input_csv_path, activities_to_remove=None):
                 trace_sentence = trace_template.format(**trace_data)
                 sentences.append(trace_sentence)  # Add the trace sentence at the start of the new case
 
-        # Generate event-level description for each event in the current case
         event_data = {attr: row[attr] for attr in event_attributes if attr in row}
-        # Format the event-level sentence
         sentence = event_template.format(**event_data)
         sentences.append(sentence)
 
-    # Save the sentences to a text file
     output_path = create_output_file(dataset_name)
     with open(output_path, "w") as file:
         for sentence in sentences:
@@ -74,14 +52,14 @@ def load_and_convert_log_to_txt(input_csv_path, activities_to_remove=None):
 
 
 def create_output_file(dataset_name):
-    directory_path = "/Users/alessandro/PycharmProjects/Tirocinio/Output"
+    directory_path = "/Dataset"
     file_path = os.path.join(directory_path, f"{dataset_name}.txt")
     print(f"File successfully created at: {file_path}")
     return file_path
 
 
 load_and_convert_log_to_txt(
-    input_csv_path="/Users/alessandro/PycharmProjects/Tirocinio/Dataset/sepsis.csv",
+    input_csv_path="/FileCSV/sepsis.csv",
     # activities_to_remove=["Activity A", "Activity B"]
 )
 
