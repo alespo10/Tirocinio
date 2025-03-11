2ecadd64298a312b27d70e62b3dfac7802ed445e
alespo10 <alesposato15@gmail.com>
2025-03-11 11:15:01 +0100

    Metodi nuovi + Test evaluation

--- evaluate.py
+++ evaluate.py
@@ -4,16 +4,14 @@ from Finetuning.dataset.pm_dataset import PMDataset
 from Finetuning.generators.complete_sequences import complete_sequences
 from peft import PeftModel
 
+# Percorso della cartella dove è salvato il tuo adapter LoRA
 
-model_path = "/kaggle/working/Tirocinio/trained_models"
+model_path = "/kaggle/working/Tirocinio/trained_models/opt_sequencer_ft_clean_fine_tuned_split_part1"
+tokenizer_path = "/kaggle/working/Tirocinio/trained_models/tokenizer_opt_sequencer_ft_clean_split_part1.tk"
+base_model = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b")
+model = PeftModel.from_pretrained(base_model, model_path)
 
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-base_model_name = "facebook/opt-1.3b"
-model = AutoModelForCausalLM.from_pretrained(base_model_name)
-
-model = PeftModel.from_pretrained(model, model_path, is_trainable=False)
-
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 model.eval()
 
 print("✅ Modello LoRA caricato correttamente!")
